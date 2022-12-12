4b13869c4c68ff4a5cc3ccafe63a95e8d2f0a916
rabiatabbasam <rabiatabbasam@yahoo.com>
2022-12-12 11:11:30 +0000

    day39: Recap

---
+++ src/day39_Recap/cydeoTask/Tester.java
@@ -0,0 +1,30 @@
+package day39_Recap.cydeoTask;
+
+public class Tester extends Employee {
+
+    public Tester(String name, int age, char gender, int employeeID, String jobTitle, double salary) {
+        super(name, age, gender, employeeID, jobTitle, salary);
+    }
+
+    public void createTicket(){
+        System.out.println(getJobTitle() + " "+getName()+" is creating ticket");
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

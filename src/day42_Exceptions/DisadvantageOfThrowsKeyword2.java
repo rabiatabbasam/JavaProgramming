46cb1fa9ebd725350e362525d5026cb2fd43c5d7
rabiatabbasam <rabiatabbasam@yahoo.com>
2022-12-25 10:16:47 +0000

    day42: Exceptions Continue

--- src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
@@ -5,30 +5,45 @@ import java.io.FileNotFoundException;
 
 public class DisadvantageOfThrowsKeyword2 {
 
+    public static void main(String[] args)throws InterruptedException {
+        method1();
+        method2();
+        method3();
+
+        String str = null;
+        try {
+            System.out.println(str.charAt(0));
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+    }
+
+
     public static  void method(){
         try {
             method1();
-        } catch (FileNotFoundException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
 
 
-    public static void method1() throws FileNotFoundException {
-        new FileInputStream("");
+    public static void method1() throws InterruptedException {
+        //new FileInputStream("");
+        Thread.sleep(3000);
     }
 
-    public static void method2() throws FileNotFoundException{
+    public static void method2() throws InterruptedException{
         method1();
     }
 
-    public static void method3() throws FileNotFoundException,InterruptedException{
+    public static void method3() throws InterruptedException{
         method2();
         Thread.sleep(1000);
     }
 
-    public static void method4()throws FileNotFoundException,InterruptedException{
+    public static void method4()throws InterruptedException{
         method3();
     }
 

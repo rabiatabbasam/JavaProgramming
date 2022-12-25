253bf5e86972c4bdab00b49d4e1334687cdfe3b7
rabiatabbasam <rabiatabbasam@yahoo.com>
2022-12-25 09:52:15 +0000

    day42: Exceptions Continue

---
+++ src/day42_Exceptions/ThrowsKeyword2.java
@@ -0,0 +1,24 @@
+package day42_Exceptions;
+
+public class ThrowsKeyword2 {
+    public static void main(String[] args) throws ArithmeticException {
+        System.out.println("Test Started");
+
+        System.out.println(8/0);
+
+        System.out.println("Test Completed");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

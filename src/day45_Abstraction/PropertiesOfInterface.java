95eda918f4454e17dd7dbf92eb56e6a1d5fddd05
Rabia Tabassum <rabiatabbasam@yahoo.com>
2022-12-30 13:32:31 +0000

    day45: Abstraction - Interface

---
+++ src/day45_Abstraction/PropertiesOfInterface.java
@@ -0,0 +1,35 @@
+package day45_Abstraction;
+
+public interface  PropertiesOfInterface {
+
+    int a = 100;//static & final by default
+    static int b = 200;//final by default
+   /*
+    public PropertiesOfInterface(int a){
+        this.a = a;
+    }
+
+    static {
+        b = 100;
+    }
+    */
+
+    /*
+    public void method1(){
+        System.out.println("Instance method");
+    }
+    */
+
+
+    static void method2(){
+        System.out.println("Static method");
+    }
+     void method3();
+
+    default void method(){
+       System.out.println("Default method");
+   }
+
+    }
+
+

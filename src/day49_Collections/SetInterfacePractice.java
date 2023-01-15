d64ba935e598f404682c42178f922a75487ccd2e
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-15 14:40:11 +0000

    day49:  Collections continue

---
+++ src/day49_Collections/SetInterfacePractice.java
@@ -0,0 +1,45 @@
+package day49_Collections;
+
+import java.util.*;
+
+public class SetInterfacePractice {
+
+    public static void main(String[] args) {
+
+       List<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(null,null,null));
+        System.out.println("list = " + list);
+
+        System.out.println(list.get(4));
+
+
+        //Reference type       object
+        Set<Integer> hashSet = new HashSet<>();
+        hashSet.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet.addAll(Arrays.asList(null,null,null));
+        System.out.println(hashSet);
+
+        Set<Integer>linkedHashSet = new LinkedHashSet<>();
+
+
+
+        Set<Integer> treeSet = new TreeSet<>();
+
+
+
+
+
+
+
+
+    }
+
+}

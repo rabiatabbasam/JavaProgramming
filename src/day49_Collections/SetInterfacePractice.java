5163e1bc6823e7fe5b1b62704926a988d17b9507
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-15 14:54:59 +0000

    day49:  Collections continue

--- src/day49_Collections/SetInterfacePractice.java
+++ src/day49_Collections/SetInterfacePractice.java
@@ -28,12 +28,25 @@ public class SetInterfacePractice {
         System.out.println(hashSet);
 
         Set<Integer>linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.addAll(Arrays.asList(10,200,300,40,90));
+        linkedHashSet.addAll(Arrays.asList(10,200,300,40,90));
+        linkedHashSet.addAll(Arrays.asList(10,200,300,40,90));
+        linkedHashSet.addAll(Arrays.asList(10,200,300,40,90));
+        linkedHashSet.addAll(Arrays.asList(null,null,null));
 
-
+        System.out.println("linkedHashSet = " + linkedHashSet);
 
         Set<Integer> treeSet = new TreeSet<>();
+        treeSet.addAll(Arrays.asList(10,200,300,40,90));
+        treeSet.addAll(Arrays.asList(10,200,300,40,90));
+        treeSet.addAll(Arrays.asList(10,200,300,40,90));
+        treeSet.addAll(Arrays.asList(10,200,300,40,90));
+       // treeSet.addAll(Arrays.asList(null,null,null));
 
+        System.out.println("treeSet = " + treeSet);
 
+      //String str = null;
+        //System.out.println(str.charAt(0));
 
 
 

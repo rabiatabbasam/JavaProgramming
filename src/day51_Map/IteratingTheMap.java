2c2015e3148d3defd9356e70e26e5a350fc30979
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-23 11:42:02 +0000

    day51: Maps Intro

---
+++ src/day51_Map/IteratingTheMap.java
@@ -0,0 +1,73 @@
+package day51_Map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class IteratingTheMap {
+
+    public static void main(String[] args) {
+        Map<String, Integer> students = new HashMap<>();
+        students.put("Aygun", 95);
+        students.put("Maria", 90);
+        students.put("Ali",85);
+        students.put("Alex",80);
+        students.put("Ozan",75);
+        students.put("Serkin",70);
+        students.put("Andriy",98);
+
+        System.out.println(students);
+
+        /*
+      Set<String> keys =  students.keySet();
+      for (String eachKey : keys) {
+           // System.out.println(eachKey +" : "+students.get(eachKey));
+        students.replace(eachKey, students.get(eachKey)+ 5);
+
+        }
+
+      */
+
+       Map<String, Integer> earlyBird = new HashMap<>();// score >= 90
+        Map<String, Integer> angryBird = new HashMap<>();//score <= 90
+
+        for (String key : students.keySet()) {
+            Integer value = students.get(key);
+           if(value  >= 90){
+               earlyBird.put(key, value);
+           }else{
+               angryBird.put(key,value);
+           }
+        }
+
+        System.out.println("earlyBird = " + earlyBird);
+        System.out.println("angryBird = " + angryBird);
+
+      Set<String> names =   students.keySet();
+
+        System.out.println("---------------------------------------");
+
+
+
+
+        // remove all the students whose score is less than 90
+        for (String eachKey : students.keySet()) {
+
+            Integer eachValue = students.get(eachKey);
+            if(eachValue < 90){
+                System.out.println(eachKey);
+            }
+
+        }
+
+        System.out.println(students);
+
+
+
+
+
+
+
+
+    }
+}

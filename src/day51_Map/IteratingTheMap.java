ea0b4a50e931d5474adeeff6f3cf2262ec615604
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-23 12:05:20 +0000

    day51: Maps Intro

--- src/day51_Map/IteratingTheMap.java
+++ src/day51_Map/IteratingTheMap.java
@@ -78,21 +78,35 @@ public class IteratingTheMap {
         System.out.println("max = " + max);
         System.out.println("min = " + min);
 
+        System.out.println("--------------------------------");
+
+        //how many students has the score of 95 or greater
+
+        int count = 0;
+       /*
+        for (Integer eachScore : students.values()) {
+         if(eachScore >= 95){
+             count++;
+         }
+        }
+      */
+
+        for (String eachKey : students.keySet()) {
+            Integer eachScore = students.get(eachKey);
+            if(eachScore >= 95){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        System.out.println("---------------------------------");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for (Map.Entry<String, Integer> entry : students.entrySet()) {
+            //System.out.println(entry);
+            System.out.println(entry.getKey()+ " : "+entry.getValue());
+        }
 
 
 

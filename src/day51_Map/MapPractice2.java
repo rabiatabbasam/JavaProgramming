a6df66b8f97417e5403cf0e3feb45c349fd91a8f
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-23 13:53:07 +0000

    day51: Maps Intro

--- src/day51_Map/MapPractice2.java
+++ src/day51_Map/MapPractice2.java
@@ -18,9 +18,12 @@ public class MapPractice2 {
         map.put("Anderson",135000);
         map.put("Steven",117000);
 
-        //1.1 who has the maximum salary
+        //1.1.2 who has the maximum and minimum salary?
         String name1 = "";
         int maxSalary = Integer.MIN_VALUE;
+
+        String name2 = "";
+        int minSalary = Integer.MAX_VALUE;
 
         for (Map.Entry<String, Integer> pair : map.entrySet()) {
             String eachKey = pair.getKey();
@@ -30,21 +33,51 @@ public class MapPractice2 {
                 name1 = eachKey;
 
             }
+            if(eachValue < minSalary){
+                minSalary = eachValue;
+                name2 = eachKey;
+            }
         }
 
         System.out.println(name1);
+        System.out.println(name2);
 
+        System.out.println("-------------------------------------------");
 
+        //1.3 how many employees has the salary between $120k  ~ 150k?
 
+        int count = 0;
+        for (Integer eachValue : map.values()) {
+            if(eachValue >=120000 && eachValue <= 150000){
+                count++;
+            }
 
+        }
+        System.out.println(count);
 
+        System.out.println("-------------------------------------");
 
+        //1.4 display the names of the employees who are making less than 118k?
 
+        for (Map.Entry<String, Integer> pairs : map.entrySet()) {
+            if(pairs.getValue() < 118000){
+                System.out.println(pairs.getKey());
+            }
 
+        }
 
+        System.out.println("--------------------------");
 
+     //1.5 increase the salary of each employee by 10k if the current salary of employee is less than 120k
 
+        System.out.println(map);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() < 120000) {
+                entry.setValue(entry.getValue() + 10000);
+            }
+        }
 
+        System.out.println(map);
 
 
 

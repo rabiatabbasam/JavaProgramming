17e2a2ec5a9a309cad4f880a40b2f70fdcc63c79
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-25 17:06:06 +0000

    day53: Functional Interface

--- src/day53_FunctionalInterface/StreamPractice.java
+++ src/day53_FunctionalInterface/StreamPractice.java
@@ -52,16 +52,26 @@ public class StreamPractice {
 
         System.out.println(evens);
 
+        List<String> names = new ArrayList<>(Arrays.asList("Java","java","jAvA","Python","Ruby"));
+      long countJava = names.stream().filter(p -> p.equalsIgnoreCase("java")).count();
+
+        System.out.println(countJava);
 
 
+        names.stream().filter(p -> p.equalsIgnoreCase("java")).forEach(p-> System.out.println(p));
 
+        List<Integer> list6 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+       boolean r1 = list6.stream().allMatch(p-> p%2==0);
 
+        System.out.println(r1);
 
+        boolean r2 = list6.stream().anyMatch(p-> p >20);
 
+        System.out.println(r2);
 
+        boolean r3 = list6.stream().noneMatch(p->p%3==0);
 
-
-
+        System.out.println(r3);
 
 
 

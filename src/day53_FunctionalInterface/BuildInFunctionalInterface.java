5d17f6e32e25f1a2fe644de8aac1ce5035696d7e
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-25 11:19:30 +0000

    day53: Functional Interface

---
+++ src/day53_FunctionalInterface/BuildInFunctionalInterface.java
@@ -0,0 +1,87 @@
+package day53_FunctionalInterface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class BuildInFunctionalInterface {
+
+    public static void main(String[] args) {
+
+
+        Predicate<String> isPalindrome = (p) -> {
+            String reverse = new StringBuilder(p).reverse().toString();
+             return reverse.equalsIgnoreCase(p);
+        };
+
+        boolean result1 = isPalindrome.test("Java");
+
+        System.out.println(result1);
+
+        Predicate<Integer> isEven = p -> p %2==0;
+
+        System.out.println("--------------------------------");
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2,3));
+        list.removeIf(isEven);
+
+        System.out.println(list);
+
+        System.out.println("--------------------------------");
+
+        List<String> names  = new ArrayList<>();
+        names.addAll(Arrays.asList("Level", "Anna","Java", "Python"));
+
+        names.removeIf(isPalindrome);
+
+        System.out.println(names);
+
+        System.out.println("------------------------------------");
+
+        Consumer<String> printEach = (s) -> {
+            for (int i = 0; i < s.length(); i++) {
+                System.out.println(s.charAt(i));
+            }
+        };
+
+        printEach.accept("Java");
+
+        System.out.println("--------------------------------");
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+       /*
+        for (Integer each : list2) {
+            if(each %2 !=0){
+                System.out.println(each);
+            }
+        }
+        */
+
+      list2.forEach(p-> {if(p%2!=0) System.out.println(p);});
+
+        System.out.println("-----------------------------");
+
+        List<String> employees = new ArrayList<>();
+        employees.addAll(Arrays.asList("Anil Adilbayeva","Aygun Abbasova Jarullah",
+                "Julili Jiwuer","Ali Ismayilov"));
+
+        employees.forEach(s -> System.out.println(s.charAt(0)+"."+s.charAt(s.lastIndexOf(" ")+1)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
